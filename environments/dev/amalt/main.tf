84c5a9c5daf46f0a64040cc45df916c233c40f1b
amaal07 <85248559+amaal07@users.noreply.github.com>
2026-04-10 05:18:10 +0530

    feat: terraform for dev/amalt

--- environments/dev/amalt/main.tf
+++ environments/dev/amalt/main.tf
@@ -8,13 +8,13 @@ terraform {
 provider "azurerm" { features {} }
 
 resource "azurerm_resource_group" "main" {
-  name     = "${var.naming_prefix}-rg"
-  location = var.region
+  name     = "AmalRG"
+  location = "East US"
 }
 
 resource "azurerm_virtual_network" "main" {
-  name                = "${var.naming_prefix}-vnet"
-  location            = var.region
+  name                = "amal-vnet"
+  location            = "East US"
   resource_group_name = azurerm_resource_group.main.name
   address_space       = ["10.0.0.0/16"]
 }
