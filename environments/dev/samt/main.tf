61debe9c4b077ef483dc629e285c513ebe5ffd1e
amaal07 <85248559+amaal07@users.noreply.github.com>
2026-04-10 05:27:31 +0530

    feat: terraform for dev/samt

---
+++ environments/dev/samt/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = { source = "hashicorp/azurerm", version = "~> 3.0" }
+  }
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "AmalRG"
+  location = var.region
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "Amal-vnet"
+  location            = var.region
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = ["10.0.0.0/16"]
+}
