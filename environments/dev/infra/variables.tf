6cd995bc632d05918a822764503bcb96a01ca7b7
amaal07 <85248559+amaal07@users.noreply.github.com>
2026-04-10 01:56:24 +0530

    feat: add variables.tf for dev/infra

--- environments/dev/infra/variables.tf
+++ environments/dev/infra/variables.tf
@@ -1,11 +1,2 @@
-variable "resource_group_name" {
-  description = "The name of the resource group."
-  type        = string
-  default     = "my-resource-group" # Replace with your desired default
-}
-
-variable "location" {
-  description = "The Azure region where the resource group should be created."
-  type        = string
-  default     = "eastus"
-}
+variable "naming_prefix" { default = "infra" }
+variable "region" { default = "eastus" }
