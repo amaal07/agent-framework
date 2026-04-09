e5fa0c6f417b1469c9ec31172629f6eb7ea2c07d
amaal07 <85248559+amaal07@users.noreply.github.com>
2026-04-10 05:12:29 +0530

    feat: terraform for dev/amalt1

---
+++ environments/dev/amalt1/variables.tf
@@ -0,0 +1,8 @@
+variable "naming_prefix" {
+  description = "Prefix for all resource names"
+  default     = "test"
+}
+variable "region" {
+  description = "Azure region"
+  default     = "East US"
+}
