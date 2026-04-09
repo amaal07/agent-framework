e8a334b9df1885ae6b1903a82aa27aba45e91fcb
amaal07 <85248559+amaal07@users.noreply.github.com>
2026-04-10 01:56:23 +0530

    feat: add main.tf for dev/infra

--- environments/dev/infra/main.tf
+++ environments/dev/infra/main.tf
@@ -1,17 +1,12 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.37"
-    }
+    azurerm = { source = "hashicorp/azurerm", version = "~> 3.0" }
   }
+  backend "azurerm" {}
 }
-
-provider "azurerm" {
-  features {}
-}
+provider "azurerm" { features {} }
 
 resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "${var.naming_prefix}-rg"
+  location = var.region
 }
