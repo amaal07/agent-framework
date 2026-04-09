3336d76b81602ee636e9ae2cc21c85d7cecbac32
amaal07 <85248559+amaal07@users.noreply.github.com>
2026-04-10 01:56:25 +0530

    feat: add outputs.tf for dev/infra

--- environments/dev/infra/outputs.tf
+++ environments/dev/infra/outputs.tf
@@ -1,9 +1,1 @@
-output "resource_group_id" {
-  value       = module.resource_group.resource_group_id
-  description = "The ID of the resource group."
-}
-
-output "resource_group_name" {
-  value       = module.resource_group.resource_group_name
-  description = "The name of the resource group."
-}
+output "resource_group" { value = azurerm_resource_group.main.name }
