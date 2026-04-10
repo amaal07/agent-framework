29f1663b3aef4ce37d278a46484d0c3701a9fac2
amaal07 <85248559+amaal07@users.noreply.github.com>
2026-04-10 05:33:26 +0530

    feat: terraform for dev/samt

--- environments/dev/samt/main.tf
+++ environments/dev/samt/main.tf
@@ -10,12 +10,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "main" {
-  name     = "AmalRG"
+  name     = "amaltest-rg"
   location = var.region
 }
 
 resource "azurerm_virtual_network" "main" {
-  name                = "<naming_prefix>-vnet"
+  name                = "amaltest-vnet"
   location            = var.region
   resource_group_name = azurerm_resource_group.main.name
   address_space       = ["10.0.0.0/16"]
