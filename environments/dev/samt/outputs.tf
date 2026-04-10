cbe44ed20d198356ab045b85da81c84145f576c7
amaal07 <85248559+amaal07@users.noreply.github.com>
2026-04-10 05:37:49 +0530

    feat: terraform for dev/samt

--- environments/dev/samt/outputs.tf
+++ environments/dev/samt/outputs.tf
@@ -1,3 +1,11 @@
 output "resource_group_name" {
   value = azurerm_resource_group.main.name
 }
+
+output "sql_database_name" {
+  value = azurerm_sql_database.main.name
+}
+
+output "sql_server_name" {
+  value = azurerm_sql_server.main.name
+}
